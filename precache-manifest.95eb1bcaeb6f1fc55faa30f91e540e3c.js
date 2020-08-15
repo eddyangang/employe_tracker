94055dd4f6147d6ceafac18d0832614eb588e36e
eddyangang <eddyyang@berkeley.edu>
2020-08-14 21:03:03 -0700

    Updates

---
+++ precache-manifest.95eb1bcaeb6f1fc55faa30f91e540e3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f717796185b2260327e71c7a464df821",
+    "url": "/employe_tracker/index.html"
+  },
+  {
+    "revision": "7e850224aa443ee90d66",
+    "url": "/employe_tracker/static/css/main.58262ad8.chunk.css"
+  },
+  {
+    "revision": "a674a81c2c4deabbd20c",
+    "url": "/employe_tracker/static/js/2.c851caee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/employe_tracker/static/js/2.c851caee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e850224aa443ee90d66",
+    "url": "/employe_tracker/static/js/main.77f750f9.chunk.js"
+  },
+  {
+    "revision": "013ec235a6c66445f442",
+    "url": "/employe_tracker/static/js/runtime-main.4b2eacc6.js"
+  }
+]);
